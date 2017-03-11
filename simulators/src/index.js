8796a91e35970fdd1dded6ec43943e56491e18fd
Jamie Nuttall <jamie@wattu.com>
2017-03-11 12:47:29 +0000

    bootstrap the car sim

--- simulators/src/index.js
+++ simulators/src/index.js
@@ -1,6 +1,11 @@
 import Sim900Stub from './sim900stub';
+import CarSim from './carsim';
 
 const HOST = process.env.HOST     || 'localhost';
 const PORT = process.env.PORT || 2222;
 
-new Sim900Stub({host :HOST, port: PORT}).connect();
+//new Sim900Stub({host :HOST, port: PORT}).connect();
+
+new CarSim().start(()=> {
+    console.log('Connected');
+});
