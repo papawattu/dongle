b412322463a1d0d384ab373d2e32b0f6257511cb
Jamie Nuttall <jamie@wattu.com>
2017-04-13 21:09:31 +0100

    added ping

--- src/vehicle/client.js
+++ src/vehicle/client.js
@@ -5,11 +5,14 @@ export default class Client {
         this.net = net;
         this.socket = null;
         this.receive = receive;
+        this.seq = 0;
+        this.chksum = 0xfd;
     }
     connect(cb) {
         console.log('Connecting to : ' + this.host + ' : ' + this.port);
         this.socket = this.net.connect({host: this.host, port: this.port},()=> {
             console.log('Connected');
+            this.startPing();
             this.socket.on('data', this.receive);
             this.socket.on('error', (err) => {
                 console.log('Error ' + err);
@@ -22,9 +25,16 @@ export default class Client {
     }
     send(data) {
         this.socket.write(data);
-        this.socket.end();
+      //  this.socket.end();
     }
     close() {
         this.socket.end();
+    }
+    startPing() {
+        setInterval(() => {
+            this.socket.write(Buffer.from([0xf9,0x04,0x00,this.seq,0x00,this.chksum]));
+            this.seq = (this.seq + 1) % 0xff;
+            this.chksum = (this.chksum + 1) % 0xff;
+        },100);
     }
 }
